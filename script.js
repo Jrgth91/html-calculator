7333149a1404389646c005deb288ce6b88414044
Jr <118298017+Jrgth91@users.noreply.github.com>
2023-11-01 22:42:39 -0400

    add more calculator buttons and functions

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
+
 const container = document.querySelector(".buttonsContainer");
 let display = document.querySelector(".displayText");
 let firstNum;
 var secNum;
-let thirdNum;
-let fourthNum;
-let fifthNum;
 let operator;
 let calc;
 
-
+// Dynamically adds divs for button layout
 function createDivs(rows, divs) {
     for(let i = 0; i < rows; i++) {
         const rowDiv = document.createElement('div');
@@ -19,37 +17,48 @@ function createDivs(rows, divs) {
             colDiv.className = `box`;
             colDiv.id = `boxNumber`;
             rowDiv.appendChild(colDiv);
-            
-           
         }   
     }
 }
 
+// Adds text and data to buttons needed for display, functions and logic
 function nameButtons() {
-    box[0].textContent = "1";
-    box[1].textContent = "2";
-    box[2].textContent = "3";
-    box[3].textContent = "+";
-    box[3].id = "boxAdd";
+    box[0].textContent = "7";
+    box[1].textContent = "8";
+    box[2].textContent = "9";
+    box[3].textContent = "/";
+    box[3].id = "boxDivide";
     box[4].textContent = "4";
     box[5].textContent = "5";
     box[6].textContent = "6";
-    box[7].textContent = "-";
-    box[7].id = "boxSubtract";
-    box[8].textContent = "7";
-    box[9].textContent = "8";
-    box[10].textContent = "9";
-    box[11].textContent = "*";
-    box[11].id = "boxMultiply";
+    box[7].textContent = "*";
+    box[7].id = "boxMultiply";
+    box[8].textContent = "1";
+    box[9].textContent = "2";
+    box[10].textContent = "3";
+    box[11].textContent = "-";
+    box[11].id = "boxSubtract";
     box[12].textContent = "0";
-    box[13].textContent = "C";
-    box[13].id = "boxClear";
+    box[13].textContent = ".";
+    box[13].id = "boxDecimal";
     box[14].textContent = "=";
     box[14].id = "boxEqual";
-    box[15].textContent = "/";
-    box[15].id = "boxDivide";
+    box[15].textContent = "+";
+    box[15].id = "boxAdd";
+    box[16].textContent = "AC";
+    box[16].id = "boxClear"
+    box[17].textContent = "C";
+    box[17].id = "boxClear";
+    box[18].textContent = "<";
+    box[18].id = "boxBack"
+    box[19].textContent = "E";
+    box[19].dataset.name = "^";
+    box[19].id = "boxExponent";
+
 }
 
+// Adds event listeners to buttons 
+// There should be a few ways to shorten this. 
 function buttonClicked() {
     box.forEach(boxes => boxes.addEventListener("mousedown", function(e){
         boxes.classList.add("clicked");
@@ -57,36 +66,48 @@ function buttonClicked() {
     box.forEach(boxes => boxes.addEventListener("mouseup", function() {
         boxes.classList.remove("clicked");
         if (boxes.id === "boxNumber") {
-            getNumbers(boxes);
+            getNumbers(boxes, "number");
+
+        } else if (boxes.id === "boxDecimal") {
+            getNumbers(boxes, "decimal")
 
         } else if (boxes.id === "boxAdd") {
-            logic(boxes, "clickLogic");
+            logic(boxes.textContent, "clickLogic");
 
         } else if (boxes.id === "boxSubtract") {
-            logic(boxes, "clickLogic");
+            logic(boxes.textContent, "clickLogic");
 
         } else if (boxes.id === "boxMultiply") {
-            logic(boxes, "clickLogic");
+            logic(boxes.textContent, "clickLogic");
 
         } else if (boxes.id === "boxDivide") {
-            logic(boxes, "clickLogic");
+            logic(boxes.textContent, "clickLogic");
 
-        } else if (boxes.id === "boxEqual") {
-            if (operator === "+" && secNum !== undefined) {
+        } else if (boxes.id === "boxExponent") {
+            logic(boxes.dataset.name, "clickLogic");
+
+        } else if (boxes.id === "boxEqual"  && secNum !== undefined) {
+            convertStrings();
+            if (operator === "+") {
                 calc = add();
-                logic(boxes, "operatorLogic");
+                logic(boxes.textContent, "operatorLogic");
 
-            } else if (operator === "-" && secNum !== undefined) {
+            } else if (operator === "-") {
                 calc = sub();
-                logic(boxes, "operatorLogic");
+                logic(boxes.textContent, "operatorLogic");
 
-            } else if (operator === "*" && secNum !== undefined) {
+            } else if (operator === "*") {
                 calc = mult();
-                logic(boxes, "operatorLogic");
+                logic(boxes.textContent, "operatorLogic");
 
-            } else if (operator === "/" && secNum !== undefined) {
+            } else if (operator === "/") {
                 calc = div();
-                logic(boxes, "operatorLogic");
+                logic(boxes.textContent, "operatorLogic");
+
+            } else if (operator === "^") {
+                calc = exponent();
+                logic(boxes.dataset.name, "operatorLogic");
+
             } 
 
         } else if (boxes.id === "boxClear") {
@@ -94,12 +115,10 @@ function buttonClicked() {
             calc = undefined;
             display.textContent = "0";
         }
-
-
-
     }))
 }
 
+// Math functions
 function add() {
     let result = firstNum + secNum;
     return Math.round(result * 100) / 100;
@@ -121,66 +140,132 @@ function div() {
     return Math.round(result * 100) / 100;
 }
 
+function exponent() {
+    result = firstNum ** secNum;
+    return Math.round(result * 100) / 100;
+}
+
 function reset() {
     firstNum = undefined;
     secNum = undefined;
     operator = undefined;
 }
 
+// Some display logic to prevent doubles of the operator between numbers
+// and display errors when calculated number is longer than the  display
+
 function logic(boxes, option) {
     if (option === "clickLogic") {
         if (firstNum !== null && secNum === undefined && operator === undefined) {
             if (firstNum !== undefined) {
-                operator = `${boxes.textContent}`;
-                display.textContent += ` ${operator} `;
-                console.log(firstNum);
-            }
-        }
+                operator = `${boxes}`;
+                display.textContent = `${firstNum} ${operator} `;
+            } 
+        } else if ( operator !== undefined) {
+            
+             operator = `${boxes}`;
+             display.textContent = `${firstNum} ${operator} `;
+             if (secNum !== undefined) {
+            operator = `${boxes}`;
+             display.textContent = `${firstNum} ${operator} ${secNum}`;
+             }
+        } 
     } else if (option === "operatorLogic") {
-        console.log("hello");
         if (calc.toString().length > 10) {
-            display.textContent = "Invalid Number"
+            display.textContent = "Error"
+            calc = undefined;
+            
         } else {
             display.textContent = calc;
         }
         reset();
         firstNum = calc;
     }
-    
-
 }
 
-function getNumbers(item) {
-    let number = parseInt(item.textContent);
+
+// This function prevents multiple decimals to be displayed aswell as
+// update the display, this can definetly be shortened
+function getNumbers(item, property) {
+    let number = item.textContent;
     if (operator === undefined && firstNum !== null) {
         if (firstNum === undefined) {
-            firstNum = number;
-            display.textContent = firstNum;
-            //return firstNum;
-            //console.log(number);
+            if (property === "decimal") {
+                firstNum = 0;
+                firstNum += ".";
+                display.textContent = firstNum;
+                // firstNum = parseFloat(firstNum);
+            } else {
+                firstNum = number;
+                display.textContent = firstNum;
+                console.log(typeof firstNum);
+                //return firstNum;
+                //console.log(number);
+            }
+
         } else {
-            firstNum = parseInt(`${firstNum}${number}`);
-            console.log(typeof firstNum);
-            display.textContent = firstNum;
-            //return firstNum;
-            //console.log(number);
+            if (property === "decimal" && !firstNum.includes(".")) {
+                firstNum += ".";
+                display.textContent = firstNum;
+                console.log("float!")
+                // console.log(typeof firstNum) 
+
+            } else {
+                if (property !== "decimal") {
+                    firstNum += number;
+                    display.textContent = firstNum;
+                    //return firstNum;
+                    //console.log(number);
+                }
+            }
         }
+    
     } else { 
         if (secNum === undefined) {
-            secNum = number;
-            display.textContent += secNum;
-            console.log(number);
+            if (property !== "decimal") {
+                secNum = number;
+                display.textContent += secNum;
+                console.log(number);
+            }
         } else {
-            secNum = parseInt(`${secNum}${number}`);
-            display.textContent += number;
-            //console.log(number);
+            if (property === "decimal") {
+                if (!secNum.includes("."))
+                {
+                    secNum = ".";
+                    display.textContent += secNum
+            } else {
+                display.textContent += number;
+            }
+
         }
 
     }
 }
+}
 
-createDivs(4,4);
+// converts the final display string into an array then
+// converts the data as either an interger or float for calulations
+function convertStrings() {
+    string = display.textContent;
+    const numbers = string.split(` ${operator} `);
+    if (numbers[0].includes(".")) {
+        firstNum = parseFloat(numbers[0]);
+    } else {
+        firstNum = parseInt(numbers[0]);
+    }
+    if (numbers[1].includes(".")) {
+        secNum = parseFloat(numbers[1]);
+    } else {
+        secNum = parseInt(numbers[1]);
+    }
+}
+
+// main
+createDivs(5,4);
 box = document.querySelectorAll('.box');
 nameButtons();
 display.textContent = "0";
 buttonClicked();
+
+
+
